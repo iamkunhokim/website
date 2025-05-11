70d3b68880075791aaa0847449b81133c2f62432
droak <me+git@droak.sh>
2025-05-10 19:40:51 -0700

    initial version

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '../app.css';
+	const { children } = $props();
+	export const prerender = true;
+</script>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,60 @@
+<div class="space-y-6 px-5 pb-8 text-base md:p-24 md:text-base pt-32">
+	<div class="text-2xl">Kunho Kim</div>
+	<div class="max-w-md space-y-3">
+		<p>
+			I grew up bouncing around Seoul, Saigon, and Boston. At Harvard, I
+			studied Environmental Science and Public Policy, drawn to the new
+			opportunities emerging as the Arctic opened up. I fell into the
+			ethereum rabbit hole in 2016, threw <a
+				href="https://www.facebook.com/p/Harvard-MIT-Asia-Blockchain-Conference-100042917713999/"
+				target="_blank"
+				class="underline hover:text-[#00E100] sm:text-black text-[#00E100]"
+				>one of the first blockchain conferences</a
+			> on campus, and never looked back.
+		</p>
+
+		<p>
+			After college, I started <a
+				href="https://www.albany.edu/news/86838.php"
+				target="_blank"
+				class="underline hover:text-[#00E100] sm:text-black text-[#00E100]"
+				>a crypto payments startup</a
+			> incubated at the Harvard iLab. We partnered with Korea’s biggest PC
+			Bang franchise—then were advised to shut down by the Korean SEC (fun
+			times). Since then, I’ve been in startups and VCs, and somewhere along
+			the way, ended up talking to the FBI and SEC. How wild! These days, I’m
+			continuing to work toward the vision of the world computer.
+		</p>
+
+		<p>
+			I’ve been in a wheelchair since a skiing accident in 2010. Out of
+			pure frustration, I’ve worked to make the world a bit more
+			wheelchair accessible—wrote <a
+				href="https://www.harvardmagazine.com/2015/10/20-states-on-wheels-harvard-students-wheelchair-guidebook"
+				target="_blank"
+				class="underline hover:text-[#00E100] sm:text-black text-[#00E100]"
+				>a travel guide</a
+			>
+			after a 50-day U.S. road trip, advised a few governments on wheelchair
+			accessibility, and became the first disabled Korean to
+			<a
+				href="https://www.jconline.com/story/news/local/2017/06/23/physical-disabilities-dont-stop-these-future-pilots/412090001/"
+				target="_blank"
+				class="underline hover:text-[#00E100] sm:text-black text-[#00E100]"
+				>earn a pilot’s license</a
+			>. Because... why not?
+		</p>
+
+		<p>We’re not here to sit still.</p>
+
+		<p>
+			Find me on <a
+				href="https://x.com/iamkunhokim"
+				target="_blank"
+				class="underline hover:text-[#00E100] sm:text-black text-[#00E100]"
+				>X</a
+			>
+			or email me at 0 (at) kunho (dot) kim.
+		</p>
+	</div>
+</div>
